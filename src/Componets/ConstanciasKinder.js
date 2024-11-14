c1f7c2a81558cae686c988d9b1cf0ec949956df8
Jesus Alberto Diaz Dominguez <j.albertho123@gmail.com>
2024-11-13 20:29:49 -0600

    asignacion de valores a inputs

--- src/Componets/ConstanciasKinder.js
+++ src/Componets/ConstanciasKinder.js
@@ -41,9 +41,16 @@ export default function ConstanciasKinder() {
   }
 
   const addNewStudentOnClick = async() => {
-
+    console.log(nombresAlumno);
+    console.log(primerApellidoAlumno);
+    console.log(segundoApellidoAlumno);
+    console.log(fechaNacimientoAlumno);
+    console.log(generoAlumno);
+    console.log(matriculaAlumno);
+    console.log(gradoAlumno);
+    console.log(estatusAlumno);
+    console.log(cicloEscolarAlumno);
   }
-
 
   return (
     <div className='contenedorConstanciasPrincipal'>
@@ -60,7 +67,11 @@ export default function ConstanciasKinder() {
                     <label>Nombres</label>
                   </div>
                   <div>
-                    <input type='text' />
+                    <input 
+                    type='text' 
+                    value={nombresAlumno} 
+                    onChange={nombresAlumnoOnChange}
+                    />
                   </div>
                 </div>
                 
@@ -69,68 +80,101 @@ export default function ConstanciasKinder() {
                       <label>Primer Apellido</label>
                   </div>
                   <div className='contenidoContrasena'>
-                      <input type='text'/>
+                      <input 
+                      type='text'
+                      value={primerApellidoAlumno}
+                      onChange={primerApellidoAlumnoOnChange}
+                      />
                   </div>
                 </div>
-                        <div className='IniciarSesionContraseña'>
-                        <div className='contraseña'>
-                            <label>Segundo Apellido</label>
-                        </div>
-                        <div className='contenidoContrasena'>
-                            <input type='text'/>
-                        </div>
-                        </div>
-                        <div className='IniciarSesionContraseña'>
-                        <div className='contraseña'>
-                            <label>Fecha Nacimiento</label>
-                        </div>
-                        <div className='contenidoContrasena'>
-                            <input type='text'/>
-                        </div>
-                        </div>
-                        <div className='IniciarSesionContraseña'>
-                        <div className='contraseña'>
-                            <label>Genero</label>
-                        </div>
-                        <div className='contenidoContrasena'>
-                            <input type='text'/>
-                        </div>
-                        </div>
-                        <div className='IniciarSesionContraseña'>
-                        <div className='contraseña'>
-                            <label>Matricula</label>
-                        </div>
-                        <div className='contenidoContrasena'>
-                            <input type='text'/>
-                        </div>
-                        </div>
-                        <div className='IniciarSesionContraseña'>
-                        <div className='contraseña'>
-                            <label>Grado</label>
-                        </div>
-                        <div className='contenidoContrasena'>
-                            <input type='text'/>
-                        </div>
-                        </div>
-                        <div className='IniciarSesionContraseña'>
-                        <div className='contraseña'>
-                            <label>Estatus</label>
-                        </div>
-                        <div className='contenidoContrasena'>
-                            <input type='text'/>
-                        </div>
-                        </div>
-                        <div className='IniciarSesionContraseña'>
-                        <div className='contraseña'>
-                            <label>ciclo Escolar</label>
-                        </div>
-                        <div className='contenidoContrasena'>
-                            <input type='text'/>
-                        </div>
-                        </div>
-                         <div className="contenButtonLogin">
-                        <button>Agregar Alumno</button>
-                    </div>
+
+                <div className='IniciarSesionContraseña'>
+                  <div className='contraseña'>
+                      <label>Segundo Apellido</label>
+                  </div>
+                  <div className='contenidoContrasena'>
+                      <input type='text'
+                      value={segundoApellidoAlumno}
+                      onChange={segundoApellidoAlumnoOnChange}
+                      />
+                  </div>
+                </div>
+                
+                <div className='IniciarSesionContraseña'>
+                  <div className='contraseña'>
+                      <label>Fecha Nacimiento</label>
+                  </div>
+                  <div className='contenidoContrasena'>
+                      <input type='text'
+                      value={fechaNacimientoAlumno}
+                      onChange={fechaNacimientoAlumnoOnChange}
+                      />
+                  </div>
+                </div>
+
+                <div className='IniciarSesionContraseña'>
+                  <div className='contraseña'>
+                      <label>Genero</label>
+                  </div>
+                  <div className='contenidoContrasena'>
+                      <input type='text'
+                      value={generoAlumno}
+                      onChange={generoAlumnoOnChange}
+                      />
+                  </div>
+                </div>
+
+                <div className='IniciarSesionContraseña'>
+                  <div className='contraseña'>
+                      <label>Matricula</label>
+                  </div>
+                  <div className='contenidoContrasena'>
+                      <input type='text'
+                      value={matriculaAlumno}
+                      onChange={matriculaAlumnoOnChange}
+                      />
+                  </div>
+                </div>
+
+                <div className='IniciarSesionContraseña'>
+                  <div className='contraseña'>
+                      <label>Grado</label>
+                  </div>
+                  <div className='contenidoContrasena'>
+                      <input type='text'
+                      value={gradoAlumno}
+                      onChange={gradoAlumnoOnChange}
+                      />
+                  </div>
+                </div>
+
+                <div className='IniciarSesionContraseña'>
+                  <div className='contraseña'>
+                      <label>Estatus</label>
+                  </div>
+                  <div className='contenidoContrasena'>
+                      <input type='text'
+                      value={estatusAlumno}
+                      onChange={estatusAlumnoOnChange}
+                      />
+                  </div>
+                </div>
+                
+                <div className='IniciarSesionContraseña'>
+                  <div className='contraseña'>
+                      <label>ciclo Escolar</label>
+                  </div>
+                  <div className='contenidoContrasena'>
+                      <input type='text'
+                      value={cicloEscolarAlumno}
+                      onChange={cicloEscolarAlumnoOnChange}
+                      />
+                  </div>
+                </div>
+                
+                <div className="contenButtonLogin">
+                        <button onClick={addNewStudentOnClick}>Agregar Alumno</button>
+                </div>
                 </div>
             </div>
         </div>
