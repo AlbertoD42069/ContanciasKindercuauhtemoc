1076d738083c9d742bb910f6c87acf0e7a5e38cc
Jesus Alberto Diaz Dominguez <j.albertho123@gmail.com>
2024-11-14 22:51:20 -0600

    inicio de tabla de constancias

--- src/Componets/ConstanciasKinder.js
+++ src/Componets/ConstanciasKinder.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Style/ConstanciasKinder.css';
 
+
 export default function ConstanciasKinder() {
   return (
     <div className='contanciasKinder'>
@@ -9,6 +10,26 @@ export default function ConstanciasKinder() {
     </div>
     <div className='tablaContancias'></div>
     <table>
+    <thead>
+        <tr>
+          <th>First Name</th>
+          <th colSpan={3}>opciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+        </tr>
+      </tbody>
         
     </table>
     </div>
