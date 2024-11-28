b2867841149015fd1fa0397f572c84805c0da000
Jesus Alberto Diaz Dominguez <j.albertho123@gmail.com>
2024-11-27 21:15:34 -0600

    ya se genera las constancias  de forma correcta pasandole los datos de la bd de firebase y ya estan las imagenes en el pdf, tiene detalle que coloca signo de coma donde no lo lleva

--- src/Componets/ConstanciasKinder.js
+++ src/Componets/ConstanciasKinder.js
@@ -3,7 +3,9 @@ import './Style/ConstanciasKinder.css';
 import { collection, getDocs, onSnapshot, snapshotEqual } from 'firebase/firestore';
 import { dbKinder } from '../firebase';
 import {jsPDF} from 'jspdf';
-
+import 'jspdf-autotable';
+import imageSep from './images/SEP_Logo.png';
+import secrtEducacion from './images/secrtEducacion.png';
 
 export default function ConstanciasKinder() {
     const encabezadoConstancia = {
@@ -14,47 +16,12 @@ export default function ConstanciasKinder() {
       nombreKinder: 'JARDÍN DE NIÑAS Y NIÑOS “CUAUHTÉMOC”',
       claveCentroTrabajo: "C. C. T. 07DCC1395S, RAYÓN",
       logoAño: ' "2025 año de algo" ',
-      tituloCostancia: "CONTANCIA DE INSCRIPCION",
+      tituloCostancia: "CONTANCIA DE INSCRIPCIÓN",
     };
-
-    const [wantedStudent, setWantedStudent] = useState();
-    const wantedStudentOnChange = (e) => {
-        setWantedStudent(e.target.value);
-    }
-    const searchStudentOnclick = async() => {
-        console.log(wantedStudent)
-    }
-    
-    const actualizarContancia = () => {
-        alert('contancia actualizada')
-    }
-    const eliminarContancia = () => {
-      alert('contancia eliminada')
-    }
-    
     const [collecionAlumnos, setCollecionAlumnos] = useState([]);
+    const collectionName = "alumnos"
 
     
-    const verContanciaOnClick = () => {
-      const doc = new jsPDF();
-        doc.setFontSize(10);
-        doc.text(encabezadoConstancia.secrFederalizada, 105, 15, null, null, "center");
-        doc.text(encabezadoConstancia.secrIndigena, 105, 20, null, null, "center");
-        doc.text(encabezadoConstancia.jefaturaZona, 105, 25, null, null, "center");
-        doc.text(encabezadoConstancia.zonaEscolar, 105, 30, null, null, "center");
-        doc.text(encabezadoConstancia.nombreKinder, 105, 35, null, null, "center");
-        doc.text(encabezadoConstancia.claveCentroTrabajo, 105, 40, null, null, "center");
-
-        doc.setFontSize(20);
-        doc.text(encabezadoConstancia.logoAño, 105, 75, null, null, "center");
-
-        doc.setFont("helvetica", "bold");
-        doc.text(encabezadoConstancia.tituloCostancia, 105, 100, null, null, "center");
-        doc.save("contancia_${constNombreAlumno}.pdf");
-    }
-
-    const collectionName = "alumnos"
-    const [nobreConstancia, setNombreConstancia] = useState("");
     useEffect(() => {
     const obtenerColeccion = async () => {
       try {
@@ -66,6 +33,7 @@ export default function ConstanciasKinder() {
         }));
         console.log("Documentos:", datos);
         setCollecionAlumnos(datos);
+
       } catch (error) {
         console.error("Error al obtener los datos:", error);
       }
@@ -74,14 +42,90 @@ export default function ConstanciasKinder() {
     }, []);
 
 
+    const verContanciaOnClick = (alumnoTabla) => {
+
+        const alingTextCenter = "center"
+        const nombreConstancia = alumnoTabla.curpAlumno+"_contancia.pdf";
+        const nombreConstanciaSinEspacio = nombreConstancia.replace(" ", "");
+
+      const doc = new jsPDF();
+        doc.addImage(imageSep,"PNG", 15, 10, 50, 28);
+        doc.addImage(secrtEducacion,"PNG", 150, 10, 50, 28);
+
+
+        doc.setFontSize(10);
+        doc.text(encabezadoConstancia.secrFederalizada, 105, 15, null, null, alingTextCenter);
+        doc.text(encabezadoConstancia.secrIndigena, 105, 20, null, null, alingTextCenter);
+        doc.text(encabezadoConstancia.jefaturaZona, 105, 25, null, null, alingTextCenter);
+        doc.text(encabezadoConstancia.zonaEscolar, 105, 30, null, null, alingTextCenter);
+        doc.text(encabezadoConstancia.nombreKinder, 105, 35, null, null, alingTextCenter);
+        doc.text(encabezadoConstancia.claveCentroTrabajo, 105, 40, null, null, alingTextCenter);
+
+        doc.setFontSize(20);
+        doc.text(encabezadoConstancia.logoAño, 105, 70, null, null, alingTextCenter);
+
+        doc.setFontSize(25);
+        doc.setFont("arial", "normal");
+        const cuerpoConstancia = `Por medio de la presente informo a quien corresponda que el alumno(a) ${alumnoTabla.nombres + alumnoTabla.primerApellido + alumnoTabla.segundoApellido} con matricula ${alumnoTabla.matricula}, nacido el\ ${alumnoTabla.fechaNacimiento} con la curp ${alumnoTabla.curpAlumno}. 
+        Esta actualmente INSCRITO en el ${alumnoTabla.grado} del jardin de niñas y niños "CUAUHTÉMOC" con C.C.T 07DCC1395S de educación preescolar del ciclo escolar ${alumnoTabla.cicloEscolar} el cual se encuentra ubicado en la esquina de la Primera Avenida Nte. Ote y Segunda Ote.Nte, Santo Domingo, 29740 Rayón, Chiapas.`;
+        
+        const emisionComst = `Se extiende la presente, para los fines legales que al interesado convenga con fecha de emisión el ${alumnoTabla.fechaEmisionConstancia}`;
+        const textoAjustado = doc.splitTextToSize(cuerpoConstancia, 180);
+        const textoAjustadoEmision = doc.splitTextToSize(emisionComst, 180);
+        
+        doc.setFont("helvetica", "bold");
+        doc.text(encabezadoConstancia.tituloCostancia, 105, 100, null, null, alingTextCenter);
+        doc.autoTable({
+          body:[[textoAjustado]],
+          startY:108,
+          alternateRowStyles: {
+            halign: 'justify', // Alineación horizontal al centro
+            fillColor: null,
+            fontSize: 15, // Tamaño de texto general para la tabla
+            // Sin fondo alternativo para las filas
+          },
+          bodyStyles: {
+            textColor: [0, 0, 0], // Color del texto de las celdas del cuerpo
+          },
+          margin: { 
+            top: 20, left: 30, right: 30, bottom: 10 
+          }, // Márgenes personalizados
+            
+        });
+        doc.autoTable({
+          body:[[textoAjustadoEmision]],
+          startY:180,
+          alternateRowStyles: {
+            halign: 'justify', // Alineación horizontal al centro
+            fillColor: null,
+            fontSize: 15, // Tamaño de texto general para la tabla
+            // Sin fondo alternativo para las filas
+          },
+          bodyStyles: {
+            textColor: [0, 0, 0], // Color del texto de las celdas del cuerpo
+          },
+          margin: { 
+            top: 20, left: 30, right: 30, bottom: 10 
+          }, // Márgenes personalizados
+            
+        });
+        doc.setFontSize(12);
+        doc.text('ATENTAMENTE', 105, 215, null, null, "center");
+        doc.text('__________________________________', 105, 230, null, null, "center");
+        doc.text('Lic. Adela Albina Dominguez Diaz', 105, 235, null, null, "center");
+        doc.text('Directora del jardin de niñas y niños CUAUHTÉMOC', 105, 240, null, null, "center");
+
+        doc.save(nombreConstanciaSinEspacio);
+    }
+
   return (
     <div className='contanciasKinder'>
     <div className='tituloKinder'>
         <h3>Constancias Emitidas</h3>
     </div>
     <div className='buscarAlumno'>
-        <input type='text' value={wantedStudent} onChange={wantedStudentOnChange}/>
-        <button onClick={searchStudentOnclick}> buscar alumno </button>
+        <input type='text'/>
+        <button> buscar alumno </button>
     </div>
     <div className='tablaContancias'>
     <table>
@@ -94,10 +138,25 @@ export default function ConstanciasKinder() {
       <tbody>
         {collecionAlumnos.map((alumnoTabla) => (
           <tr key={alumnoTabla.id}>
-            <td className='tituloTabla'>{alumnoTabla.nombres} {alumnoTabla.primerApellido} {alumnoTabla.segundoApellido}</td>
-            <td><button className='btn-contancia' onClick={verContanciaOnClick}>contancia</button></td>
-            <td><button className='btn-contancia' onClick={actualizarContancia}>actualizar</button></td>
-            <td><button className='btn-contancia'>eliminar</button></td>
+            <td className='tituloTabla'>
+              {alumnoTabla.nombres} {alumnoTabla.primerApellido} {alumnoTabla.segundoApellido} 
+            </td>
+            <td>
+              <button className='btn-contancia' 
+            onClick={() => verContanciaOnClick(alumnoTabla)}>
+              contancia
+              </button>
+              </td>
+            <td>
+              <button className='btn-contancia'>
+                actualizar
+                </button>
+                </td>
+            <td>
+              <button className='btn-contancia'>
+                eliminar
+                </button>
+                </td>
           </tr>
         ))}
       </tbody>
@@ -106,32 +165,3 @@ export default function ConstanciasKinder() {
     </div>
   )
 }
-/*
-collecionAlumnos.map((alumnoConst) => {
-      setNombreConstancia(alumnoConst.nombres);
-      console.log("---",nobreConstancia);
-    })
- <tr>
-          <td>Jesus Alberto Diaz dominguez</td>
-          <td><button className='btn-contancia' onClick={verContanciaOnClick}>contancia</button></td>
-          <td><button className='btn-contancia' onClick={actualizarContancia}>actualizar</button></td>
-          <td><button className='btn-contancia' onClick={eliminarContancia}>eliminar</button></td>
-        </tr>
-
-    useEffect(() => {
-      const fetchAlumnos = async () => {
-        try {
-          const getAlumnosData = collection(dbKinder, collectionName);
-          const snapshotAlumnos = await getDoc(getAlumnosData);
-          const alumnoData = snapshotAlumnos.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-          }))
-          setAlumnosTablas(alumnoData)
-        } catch (error) {
-          console.error("Error al obtener los alumnos:", error);
-        }
-      }
-      fetchAlumnos();
-    }, []);
-*/
