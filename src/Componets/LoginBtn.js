fcdadfc2e77c4a3d3f278788cc2b0baeed419159
Jesus Alberto Diaz Dominguez <j.albertho123@gmail.com>
2024-12-05 20:10:47 -0600

    segregacion de login exitosa

--- src/Componets/LoginBtn.js
+++ src/Componets/LoginBtn.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Style/Login.css';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { authKinder } from '../firebase';
 
 const LoginBtn = () => {
+
+  const [userKinder, setUserKinder] = useState();
+  const [passKinder, setPassKinder] = useState();
+
+  const authKinderUserOnChange = (e) => {
+    setUserKinder(e.target.value);
+  }
+  const authKinderPassOnChange = (e) => {
+    setPassKinder(e.target.value);
+  }
+  const authKinderUserOnClick = async () => {
+    try {
+        await signInWithEmailAndPassword(authKinder, userKinder, passKinder);
+      } catch (err) {
+        alert('Usuario y Contraseña incorrecto favor de verificar')
+      }
+  }
+
   return (
-    <div>
-      
+    <div className='contenedorPtincipalLogin'>
+        <div className='contenedorLogin'>
+            <div className='login'>
+                <div className='IniciarSesionTitulo'>
+                    <h3>INICIAR SESION</h3>
+                </div>
+                <div className='IniciarSesionForm'>
+                    <div className='IniciarSesionUsuario'>
+                         <div className='usuario'>
+                             <label>Usuario</label>
+                         </div>
+                         <div className='contenidoUsuario'>
+                             <input type='text' value={userKinder} onChange={authKinderUserOnChange}/>
+                         </div>
+                    </div>
+
+                    <div className='IniciarSesionContraseña'>
+                        <div className='contraseña'>
+                            <label>Contraseña</label>
+                        </div>
+                        <div className='contenidoContrasena'>
+                            <input type='password' value={passKinder} onChange={authKinderPassOnChange}/>
+                        </div>
+                    </div>
+                    <div className="contenButtonLogin">
+                        <button onClick={authKinderUserOnClick}>Iniciar Sesion</button>
+                    </div>
+                </div>
+               
+            </div>
+        </div>
     </div>
   )
 }
